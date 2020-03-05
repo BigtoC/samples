8f3aa6c47491a2bab761f04d0764ce623a1bb7ff
Chan Tai To <taitocdt@gmail.com>
2020-03-05 14:29:37 +0800

    Updated codes with suggested syntax

--- spring-webserver/server-kotlin/src/main/kotlin/net/corda/server/controllers/StandardController.kt
+++ spring-webserver/server-kotlin/src/main/kotlin/net/corda/server/controllers/StandardController.kt
@@ -26,27 +26,27 @@ class StandardController(rpc: NodeRPCConnection) {
     @GetMapping(value = ["/status"], produces = arrayOf("text/plain"))
     private fun status() = "200"
 
-    @GetMapping(value = ["/servertime"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/servertime"], produces = ["text/plain"])
     private fun serverTime() = LocalDateTime.ofInstant(proxy.currentNodeTime(), ZoneId.of("UTC")).toString()
 
-    @GetMapping(value = ["/addresses"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/addresses"], produces = ["text/plain"])
     private fun addresses() = proxy.nodeInfo().addresses.toString()
 
-    @GetMapping(value = ["/identities"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/identities"], produces = ["text/plain"])
     private fun identities() = proxy.nodeInfo().legalIdentities.toString()
 
-    @GetMapping(value = ["/platformversion"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/platformversion"], produces = ["text/plain"])
     private fun platformVersion() = proxy.nodeInfo().platformVersion.toString()
 
-    @GetMapping(value = ["/peers"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/peers"], produces = ["text/plain"])
     private fun peers() = proxy.networkMapSnapshot().flatMap { it.legalIdentities }.toString()
 
-    @GetMapping(value = ["/notaries"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/notaries"], produces = ["text/plain"])
     private fun notaries() = proxy.notaryIdentities().toString()
 
-    @GetMapping(value = ["/flows"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/flows"], produces = ["text/plain"])
     private fun flows() = proxy.registeredFlows().toString()
 
-    @GetMapping(value = ["/states"], produces = arrayOf("text/plain"))
+    @GetMapping(value = ["/states"], produces = ["text/plain"])
     private fun states() = proxy.vaultQueryBy<ContractState>().states.toString()
 }
